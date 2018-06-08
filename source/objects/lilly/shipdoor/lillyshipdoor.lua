caa84aefbcbea52c3d521853e691eb366f14fd44
Nire Inicana <thakyz@outlook.com>
2018-06-08 08:03:35 -0700

    Added Teleporter like ship door

---
+++ source/objects/lilly/shipdoor/lillyshipdoor.lua
@@ -0,0 +1,27 @@
+require "/scripts/util.lua"
+
+function init()
+  self.detectArea = config.getParameter("detectArea")
+  self.detectArea[1] = object.toAbsolutePosition(self.detectArea[1])
+  self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
+
+  animator.setAnimationState("portal", "off")
+
+  storage.uuid = storage.uuid or sb.makeUuid()
+  object.setInteractive(true)
+end
+
+function update(dt)
+  local players = world.entityQuery(self.detectArea[1], self.detectArea[2], {
+      includedTypes = {"player"},
+      boundMode = "CollisionArea"
+    })
+
+  if #players > 0 and animator.animationState("portal") == "off" then
+    animator.setAnimationState("portal", "open")
+    animator.playSound("on");
+  elseif #players == 0 and animator.animationState("portal") == "on" then
+    animator.setAnimationState("portal", "close")
+    animator.playSound("off");
+  end
+end
